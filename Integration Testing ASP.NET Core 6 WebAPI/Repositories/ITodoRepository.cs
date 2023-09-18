ad3ca9e24632d43dbf467ec603bf419d131eeb35
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-19 04:34:21 +0530

    works comitted

---
+++ Integration Testing ASP.NET Core 6 WebAPI/Repositories/ITodoRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Integration_Testing_ASP.NET_Core_6_WebAPI.Repositories
+{
+    public interface ITodoRepository
+    {
+        Task<IEnumerable<TodoItem>> GetTodoItemsAsync();
+        Task<TodoItem> GetTodoItemAsync(int id);
+        Task<TodoItem> AddTodoItemAsync(TodoItem todoItem);
+        Task<TodoItem> UpdateTodoItemAsync(TodoItem todoItem);
+        Task<TodoItem> DeleteTodoItemAsync(int id);
+    }
+}
---
+++ Integration Testing ASP.NET Core 6 WebAPI/Repositories/TodoRepository.cs
@@ -0,0 +1,48 @@
+﻿using Integration_Testing_ASP.NET_Core_6_WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Integration_Testing_ASP.NET_Core_6_WebAPI.Repositories
+{
+    public class TodoRepository :ITodoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TodoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TodoItem>> GetTodoItemsAsync()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        public async Task<TodoItem> GetTodoItemAsync(int id)
+        {
+            return await _context.TodoItems.FindAsync(id);
+        }
+
+        public async Task<TodoItem> AddTodoItemAsync(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+            return todoItem;
+        }
+
+        public async Task<TodoItem> UpdateTodoItemAsync(TodoItem todoItem)
+        {
+            _context.Entry(todoItem).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return todoItem;
+        }
+
+        public async Task<TodoItem> DeleteTodoItemAsync(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null) return null;
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+            return todoItem;
+        }
+    }
+}
