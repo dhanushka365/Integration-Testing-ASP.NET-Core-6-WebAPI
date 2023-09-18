d23ba3a0d711a33c29e49c0a9eea5022c05e38a9
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-18 09:53:38 +0530

    Add project files.

---
+++ Integration Testing ASP.NET Core 6 WebAPI/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/add", (int a, int b) => a + b);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
