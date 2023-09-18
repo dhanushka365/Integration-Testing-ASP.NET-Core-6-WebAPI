f1254fe2342d6d794bb4545bf19f1c91b103098e
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-18 13:14:39 +0530

    works commited

--- MinimalAPITest/ApiTests.cs
+++ MinimalAPITest/ApiTests.cs
@@ -8,28 +8,37 @@ namespace MinimalAPITest
     [TestClass]
     public class ApiTests
     {
-        public ApiTests()
+        private static HttpClient _httpClient;
+        private static WebApplicationFactory<Program> _webAppFactory;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
         {
-         
+            // Create the WebApplicationFactory and HttpClient once for all tests in this class.
+            _webAppFactory = new WebApplicationFactory<Program>();
+            _httpClient = _webAppFactory.CreateClient();
         }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            // Dispose of resources after all tests in this class are complete.
+            _httpClient.Dispose();
+            _webAppFactory.Dispose();
+        }
+
         [TestMethod]
         public async Task DefaultRoute_Returns_HelloWorld_Stream()
         {
-            var webAppFactory = new WebApplicationFactory<Program>();
-            var HttpClient = webAppFactory.CreateDefaultClient();
-
-            var response = await HttpClient.GetAsync("/");
+            var response = await _httpClient.GetAsync("/");
             var stringResult = await response.Content.ReadAsStringAsync();
             Assert.AreEqual("Hello World!", stringResult);
-            
         }
 
         [TestMethod]
-        public async Task sumValueReturn()
+        public async Task SumValue_Returns_CorrectResult()
         {
-            var webAppFactory = new WebApplicationFactory<Program>();
-            var httpClient = webAppFactory.CreateDefaultClient();
-            var response = await httpClient.GetAsync("/add?a=1&b=2");
+            var response = await _httpClient.GetAsync("/add?a=1&b=2");
             var stringResult = await response.Content.ReadAsStringAsync();
             Assert.AreEqual("3", stringResult);
         }
